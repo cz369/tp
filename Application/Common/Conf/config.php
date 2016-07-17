c20ed9ab22bc2ddfd066b166f6bd7dd5082ed77d
Chen Zhong <chenzhong@gmail.com>
2016-07-17 16:25:51 +0800

    增加登录界面

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,4 +10,10 @@ return array(
 	'DB_PREFIX'             =>  'think_',    // 数据库表前缀
 	'DB_CHARSET'            =>  'utf8',      // 数据库编码
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
+	
+    
+    //'URL_MODEL'          => '2', //URL模式
+    //'__CSS__' => __ROOT__ . '/Public/css',
+    
+    
 );
