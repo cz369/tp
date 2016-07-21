4a9c419ffa8f6ed30d3ea3c7c883e7ac71ba1f38
sunjun <mindfulstart@gmail.com>
2016-07-21 13:46:19 +0800

    增加拍照设置

--- Public/js/self.js
+++ Public/js/self.js
@@ -1,50 +1,16 @@
 function mainFunction()
 {
+    w3IncludeHTML();
     startClock();
     addBaiduMap();
-    $.get("group",function(data,status){
-        $('#tree').treeview({
-            data: data,
-            color: "#428bca",
-
-            onNodeSelected: function(event, node) {
-                // 事件代码...
-                //alert(node.text);
-
-                $.get("device_info",{device_id: node.text}, function(data,status){
-                    //alert(data[0].device_photo_size);
-                    $("#device_id").val(data[0].device_id);
-                    $("#device_photo_size").val(data[0].device_photo_size);
-                    $("#device_photo_quality").val(data[0].device_photo_quality);
-                    $("#device_photo_starttime").val(data[0].device_photo_starttime);
-                    $("#device_photo_endtime").val(data[0].device_photo_endtime);
-                    $("#device_frequency").val(data[0].device_frequency);
-                    // alert(data[0].device_photo_auto);
-                    if (data[0].device_photo_auto == "1") {
-                        $("#device_photo_auto").attr("checked", true);
-                    } else {
-                        $("#device_photo_auto").attr("checked", false);
-                    }
-
-                   var point = new BMap.Point(data[0].longitude, data[0].latitude)
-
-                    var map = new BMap.Map("allmap");    // 创建Map实例
-                    map.centerAndZoom(point, 11);  // 初始化地图,设置中心点坐标和地图级别
-                    map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-                    map.setCurrentCity("济南");          // 设置地图显示的城市 此项是必须设置的
-                    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-                    var mk = new BMap.Marker(point);
-                    map.addOverlay(mk);
-
-                });
-            }
-        });
-    });
+    updateTreeView();
+    addDeviceTabListener();
+    addSetting();
 }
 
 function addBaiduMap()
 {
-    var map = new BMap.Map("allmap");
+    var map = new BMap.Map("baidu-device-map");
 
     var geolocation = new BMap.Geolocation();
     geolocation.getCurrentPosition(function(r){
@@ -75,7 +41,7 @@ function startClock()
     t=setTimeout(function(){
         startClock();
     },500);
-}       
+}
 
 function changnum(i){      //月、日、秒如果小于10数字前加0
     if(i<10){
@@ -84,8 +50,105 @@ function changnum(i){      //月、日、秒如果小于10数字前加0
     return i;
 }
 
-function add(argument) {
-    
+function addDeviceTabListener() 
+{
+    $("li#device-map").click(function(){
+        $(this).attr("class", "active");
+        $("li#device-status").attr("class", "");
+        $("li#device-info").attr("class", "");
+
+        $("div#baidu-device-map").show();
+        $("div#device-status").hide()
+        $("div#device-info").hide();
+    });
+
+    $("li#device-status").click(function(){
+        $(this).attr("class", "active");
+        $("li#device-map").attr("class", "");
+        $("li#device-info").attr("class", "");
+
+        $("div#baidu-device-map").hide();
+        $("div#device-status").show()
+        $("div#device-info").hide();
+    });
+
+    $("li#device-info").click(function(){
+        $(this).attr("class", "active");
+        $("li#device-map").attr("class", "");
+        $("li#device-status").attr("class", "");
+
+        $("div#baidu-device-map").hide();
+        $("div#device-status").hide()
+        $("div#device-info").show();
+    });
+}
+
+function updateTreeView()
+{
+    $.get("group",function(data,status){
+        $('#tree').treeview({
+            data: data,
+        color: "#428bca",
+
+        onNodeSelected: function(event, node) {
+            // 事件代码...
+            //alert(node.text);
+
+            $.get("device_info",{device_id: node.text}, function(data,status){
+                //alert(data[0].device_photo_size);
+                $("#device_id").val(data[0].device_id);
+                $("#device_photo_size").val(data[0].device_photo_size);
+                $("#device_photo_quality").val(data[0].device_photo_quality);
+                $("#device_photo_starttime").val(data[0].device_photo_starttime);
+                $("#device_photo_endtime").val(data[0].device_photo_endtime);
+                $("#device_frequency").val(data[0].device_frequency);
+                // alert(data[0].device_photo_auto);
+                if (data[0].device_photo_auto == "1") {
+                    $("#device_photo_auto").attr("checked", true);
+                } else {
+                    $("#device_photo_auto").attr("checked", false);
+                }
+
+                var point = new BMap.Point(data[0].longitude, data[0].latitude)
+
+                var map = new BMap.Map("baidu-device-map");    // 创建Map实例
+            map.centerAndZoom(point, 11);  // 初始化地图,设置中心点坐标和地图级别
+            map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+            map.setCurrentCity("济南");          // 设置地图显示的城市 此项是必须设置的
+            map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+            var mk = new BMap.Marker(point);
+            map.addOverlay(mk);
+
+            });
+        }
+        });
+    });
+
+}
+
+function addSetting()
+{
+    var dialog = document.querySelector('dialog');
+    var showDialogButton = document.querySelector('#device-setting-dialog');
+    if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+    showDialogButton.addEventListener('click', function() {
+
+        dialog.showModal();
+        alert($("#device_id").val());
+        var device_id = $("#device_id").val();
+
+        $.post("__URL__/need_photo", {device_id:device_id},function(data,status){
+
+
+
+        });
+    });
+    dialog.querySelector('.mdl-button').addEventListener('click', function() {
+        dialog.close();
+        //$("#mdl-spinner").remove();
+    });
 }
 
 $(document).ready(mainFunction);
